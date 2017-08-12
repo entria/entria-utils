5d8ea25b7a32a6c1f1f889f318ac1ee91026f6de
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-08-12 09:56:44 -0300

    Add some more functions

--- src/utils/Strings.js
+++ src/utils/Strings.js
@@ -1,2 +1,13 @@
 // @flow
 export const isEmpty = (value: string): boolean => value === undefined || value === null || value === '';
+
+export const isAlpha = (value: string): boolean => /[a-zA-ZÁÉÍÓÚáéíóú]+$/.test(value);
+
+export const isEmail = (value: string): boolean => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value);
+
+export const uppercaseFirstLetter = (text: string): string => `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
+
+export const capitalize = (text: string): string =>
+  text.toLowerCase().split(' ').map(word => uppercaseFirstLetter(word)).join(' ');
+
+export const numbers = (value: string): string => value.replace(/\D/g, '');
--- src/utils/__tests__/Strings.spec.js
+++ src/utils/__tests__/Strings.spec.js
@@ -19,3 +19,31 @@ it('isEmpty - should return false if filled', () => {
 it('isEmpty - should return false if 0', () => {
   expect(Strings.isEmpty('0')).toMatchSnapshot();
 });
+
+it('isAlpha - should return true if valid', () => {
+  expect(Strings.isAlpha('Fábio Assunção')).toMatchSnapshot();
+});
+
+it('isAlpha - should return false if invalid', () => {
+  expect(Strings.isAlpha('Fábio Assunção #@*&')).toMatchSnapshot();
+});
+
+it('isEmail - should return true if valid', () => {
+  expect(Strings.isEmail('john@doe.com')).toMatchSnapshot();
+});
+
+it('isEmail - should return false if invalid', () => {
+  expect(Strings.isEmail('fake@d.c.b')).toMatchSnapshot();
+});
+
+it('uppercaseFirstLetter', () => {
+  expect(Strings.uppercaseFirstLetter('barney stinson')).toMatchSnapshot();
+});
+
+it('capitalize', () => {
+  expect(Strings.capitalize('barney stinson')).toMatchSnapshot();
+});
+
+it('numbers', () => {
+  expect(Strings.numbers('1, 2, 3, Un pasito p\' adelante. 1, 2, 3, Un pasito p\' atrás.')).toMatchSnapshot();
+});
