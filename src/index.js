6833190b7036c306391a3a0b65e514db6b5ecbd7
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-08-12 11:30:41 -0300

    Add Locale

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ export DocumentTitle from './components/DocumentTitle';
 export InfiniteScroll from './components/InfiniteScroll';
 
 export * as Colors from './utils/Colors';
+export * as Locale from './utils/Locale';
 export * as Objects from './utils/Objects';
 export * as Strings from './utils/Strings';
